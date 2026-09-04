af8b47210182425fbbbcc541fe8f2413006c6fdf
Ankit <ayankit@icloud.com>
2026-09-04 14:17:45 +0530

    Update favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,7 @@
 <script lang="ts">
 	import './layout.css';
-	import faviconIco from '$lib/assets/favicon.ico';
-	import faviconSvg from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" type="image/svg+xml" href={faviconSvg} />
-	<link rel="shortcut icon" href={faviconIco} />
-</svelte:head>
 
 {@render children()}
