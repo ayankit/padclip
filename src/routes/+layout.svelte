f3bc3a3e2928641574c3e5d1fb7152919efe4e2d
Ankit <ayankit@icloud.com>
2026-09-04 09:40:21 +0530

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import faviconIco from '$lib/assets/favicon.ico';
+	import faviconSvg from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" type="image/svg+xml" href={faviconSvg} />
+	<link rel="shortcut icon" href={faviconIco} />
+</svelte:head>
+
+{@render children()}
